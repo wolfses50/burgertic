2c903879486ebfda9ac0ccb8d8cae88429bf32ff
wolfses50 <wolfzunulises@gmail.com>
2023-09-14 21:29:07 -0300

    feat: error handling

--- controllers.js
+++ controllers.js
@@ -1,7 +1,7 @@
 const menu = require('./menu.json');
 
 const getMenu = (_, res) => {
-    res.json({
+    res.status(200).json({
         menu
     });
 }
@@ -9,39 +9,56 @@ const getMenu = (_, res) => {
 const getMenuItem = (req, res) => {
     const { id } = req.params;
     const item = menu.find(where => where.id == id);
-    res.json({
+    if (!item) {
+        res.status(404).json({
+            msg: "No se ha encontrado el item"
+        });
+        return;
+    }
+    res.status(200).json({
         item
     });
 }
 
 const getCombos = (_, res) => {
     const combos = menu.filter(where => where.tipo == "combo");
-    res.json({
+    res.status(200).json({
         combos
     });
 }
 
 const getPrincipales = (_, res) => {
     const principales = menu.filter(where => where.tipo == "principal");
-    res.json({
+    res.status(200).json({
         principales
     });
 }
 
 const getPostres = (_, res) => {
     const postres = menu.filter(where => where.tipo == "postre")
-    res.json({
+    res.status(200).json({
         postres
     });
 }
 
 const postPedido = (req, res) => {
-    const pedido = req.productos.length
-    const platos = [];
-    for (let i; i < pedido; i++) {
-        platos.push(productos[i]);
+    const { productos } = req.body;
+    const ids = productos.map(plato => plato.id);
+    const idMenu = menu.map(plato => plato.id);
+    const idNoExistente = ids.filter(id => !idMenu.includes(id));
+    if (idNoExistente.length > 0) {
+        res.status(404).json({
+            msg: `Los siguientes id's no existen: ${idsNoExistentes.join(", ")}`
+        });
+        return;
     }
-    res.json(platos);
+    const pedido = productos.map(plato => menu.find(where => where.id == plato.id));
+    const total = pedido.reduce((acc, plato) => acc + plato.precio, 0);
+
+    res.status(200).json({
+        msg: "Pedido realizado con éxito",
+        total
+    });
 }
 
 module.exports = { getMenu, getCombos, getMenuItem, getPostres, getPrincipales, postPedido }
