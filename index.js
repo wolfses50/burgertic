87a4cc6b40df7915d5c3528959404245b9d92e4c
Ulises Wolfzun <wolfzunulises@gmail.com>
2023-09-15 09:54:04 -0300

    update: formatting

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ const port = 3000;
 app.use(express.json());
 app.use(router);
 
-app.listen(port, () => {console.log(`Server running on port ${port}`);});
+app.listen(port, () => { console.log(`Server running on port ${port}`); });
